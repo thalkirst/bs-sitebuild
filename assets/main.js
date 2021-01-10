798912c2e23ef9a40eaf7d1fb46f534ec6f8e7a9
thalkirst <peter.bonyai@gmail.com>
2021-01-10 14:47:59 +0100

    tested and works as specified

--- assets/main.js
+++ assets/main.js
@@ -1,54 +1,63 @@
 $('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
       location.hostname == this.hostname
     ) {
-      
+
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      
+
       if (target.length) {
-        
+
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 300, function() {
-          
-          var $target = $(target);
+        }, 300, function () {
+
+          let $target = $(target);
           $target.focus();
-          if ($target.is(":focus")) { 
+          if ($target.is(":focus")) {
             return false;
           } else {
-            $target.attr('tabindex','-1');
-            $target.focus(); 
+            $target.attr('tabindex', '-1');
+            $target.focus();
           };
         });
       }
     }
   });
 
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+$('.js-scroll-trigger').click(function () {
+  $('.navbar-collapse').collapse('hide');
+});
 
-  const navbarCollapse = () => {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
+const navbarCollapse = () => {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+};
 
-  navbarCollapse();
+navbarCollapse();
 
-  $(window).scroll(navbarCollapse);
+$(window).scroll(navbarCollapse);
 
-  const navbar = document.querySelector('.navbar-brand');
-  navbar.addEventListener('click', () => {
-    window.scrollTo(0, 0);
-  });
+$('[data-toggle="tooltip"]').tooltip()
 
+const privacyButton = document.querySelector('.privacyButton');
+privacyButton.addEventListener('click', () => {
+  $('#privacyModal').modal('show')
+})
+
+const termsButton = document.querySelector('.termsButton');
+termsButton.addEventListener('click', () => {
+  $('#termsModal').modal('show')
+})
+
+const faqButton = document.querySelector('.faqButton');
+faqButton.addEventListener('click', () => {
+  $('#faqModal').modal('show')
+})
